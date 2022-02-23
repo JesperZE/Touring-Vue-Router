f571854ad21d96daf84a6daed1d902d1f6b326ce
jesper.eriksson <jesperzeriksson@gmail.com>
2022-02-23 19:37:19 +0100

    L4 mid

---
+++ src/views/event/Register.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>{{ event.title }}</h1>
+  <p>Register for the event here </p>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  props: ['event']
+}
+</script>

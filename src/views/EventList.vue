e7e5c1d633c9de2032cbd19897bdbe310e5adab3
jesper.eriksson <jesperzeriksson@gmail.com>
2022-02-23 18:33:30 +0100

    mid

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,31 +2,62 @@
   <h1>Events for Good</h1>
   <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <router-link
+    :to="{ name: 'EventList', query: { page: page - 1 } }"
+    rel="prev"
+    v-if="page != 1"
+    >Previous Page</router-link>
+
+    <router-link
+    :to="{ name: 'EventList', query: {page: + 1 } }"
+    rel="next"
+    v-if="hasNextPage"
+    >Next Page
+    </router-link>
+
   </div>
+
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
 
 export default {
   name: 'EventList',
+  props: ['page'],
   components: {
     EventCard
   },
   data() {
     return {
-      events: null
+      events: null,
+      totalEvents: 0, // <- Store totalEvents that are fetched from the api
+
     }
   },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
+      watchEffect(() => {
+        this.events = null 
+        EventService.getEvents(2, this.page)
+          .then(response => {
+            this.events = response.data
+            this.totalEvents = response.headers['x-total-count']
+          })
+          .catch(error => {
+            console.log(error)
+          })
       })
-      .catch(error => {
-        console.log(error)
-      })
+  },
+  computed: {
+    hasNextPage() {
+      // Calculate the total pages
+      var totalPages = Math.ceil(this.totalEvents / 2) // 2 events per page
+      
+      // Return true if the current page is less than the total pages
+      return this.page < totalPages
+    }
   }
 }
 </script>
